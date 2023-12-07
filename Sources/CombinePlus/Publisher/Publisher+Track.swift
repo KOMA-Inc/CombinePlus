9943ce4ed0f34f5a8a2697ec1dcc226b319ca15b
gloomikon <gloomikon@jatapp.com>
2023-12-07 15:37:58 +0100

    Publishers+OnError

--- Sources/CombinePlus/Publisher/Publisher+Track.swift
+++ Sources/CombinePlus/Publisher/Publisher+Track.swift
@@ -1,6 +1,15 @@
 import Combine
 
 public extension Publisher {
+
+    func onError(action: @escaping (Self.Failure) -> Void) -> Publishers.HandleEvents<Self> {
+        handleEvents(receiveCompletion: { completion in
+            if case let .failure(error) = completion {
+                action(error)
+            }
+        })
+    }
+
     func track(action: @escaping (Self.Output) -> Void) -> Publishers.HandleEvents<Self> {
         handleEvents(receiveOutput: action)
     }
